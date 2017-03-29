32fbe874ae89e9d70b0f9d42c4ad07271674cd24
Robert Goldshear <rsg34@pitt.edu>
2017-03-29 11:14:12 -0400

    made values updatable

--- TrainSystem/src/com/rogueone/ctc/gui/ChangeParametersGUI.java
+++ TrainSystem/src/com/rogueone/ctc/gui/ChangeParametersGUI.java
@@ -261,14 +261,14 @@ public class ChangeParametersGUI extends javax.swing.JFrame {
         String trainAuthorityString = SetAuthorityField.getText();
         double trainAuthority = Double.parseDouble(trainAuthorityString);
         
-        Object updateParamsID = ctcGUI.TrainTable.getValueAt(ctcGUI.TrainTable.getSelectedRow(),0);
-        Object updateParamsLine = ctcGUI.TrainTable.getValueAt(ctcGUI.TrainTable.getSelectedRow(),1);
+        Object updateParamsID = ctcGUI.TrainTable.getValueAt(ctcGUI.TrainTable.getSelectedRow(),1);
+        String updateParamsLine = (String)ctcGUI.TrainTable.getValueAt(ctcGUI.TrainTable.getSelectedRow(),0);
         String updateParamsPosition = (String)ctcGUI.TrainTable.getValueAt(ctcGUI.TrainTable.getSelectedRow(),2);
        
-            String[] parts = updateParamsPosition.split(":");
+            String[] parts = updateParamsPosition.split(" : ");
             int partBlock = Integer.parseInt(parts[1]);
             
-        trainSystem.getTrackControllerHandler().requestUpdateSpeedAuthority((Global.Line)updateParamsLine, partBlock, (byte) trainSpeed, (short) trainAuthority);
+        trainSystem.getTrackControllerHandler().requestUpdateSpeedAuthority(Global.Line.valueOf(updateParamsLine), partBlock, (byte) trainSpeed, (short) trainAuthority);
         
     }//GEN-LAST:event_UpdateButtonActionPerformed
 
