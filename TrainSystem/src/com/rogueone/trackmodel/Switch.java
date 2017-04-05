bd2b62afce6d5e4528573f82a064a925198c8e57
Dan <Dan@150.212.66.231>
2017-04-05 10:54:23 -0400

    Javadoc'd Switches

--- TrainSystem/src/com/rogueone/trackmodel/Switch.java
+++ TrainSystem/src/com/rogueone/trackmodel/Switch.java
@@ -14,11 +14,11 @@ import com.rogueone.global.Global.PieceType;
  */
 public class Switch implements TrackPiece {
     
-    private int switchID;
-    private Line line;
-    private TrackPiece portA;
-    private TrackPiece portB;
-    private TrackPiece portC;
+    private final int switchID;
+    private final Line line;
+    private final TrackPiece portA;
+    private final TrackPiece portB;
+    private final TrackPiece portC;
     private boolean isActivated;
     
     //Constructor
@@ -32,32 +32,88 @@ public class Switch implements TrackPiece {
     }
     
     //Getters & Setters
+    
+    /**
+    * Get line switch is on
+    * @author Dan Bednarczyk
+    * @return Line object switch is on
+    */
     public Line getLine() {
         return line;
     }
+    
+    /**
+    * Get portA (static block)
+    * @author Dan Bednarczyk
+    * @return TrackPiece for portA
+    */
     public TrackPiece getPortA() {
         return portA;
     }
+    
+    /**
+    * Get portB (default block)
+    * @author Dan Bednarczyk
+    * @return TrackPiece for portB
+    */
     public TrackPiece getPortB() {
         return portB;
     }
+    
+    /**
+    * Get portC (alternate block)
+    * @author Dan Bednarczyk
+    * @return TrackPiece for portC
+    */
     public TrackPiece getPortC() {
         return portC;
     }
+    
+    /**
+    * Toggle current switch state
+    * @author Dan Bednarczyk
+    */
     public void toggleSwitch() {
         isActivated = !isActivated;
     }
+    
+    /**
+    * Set switch state
+    * @author Dan Bednarczyk
+    * @param activate boolean for new switch state
+    */
     public void setSwitch(boolean activate) {
         isActivated = activate;
     }
     
     //TrackPiece interface methods
+    
+    /**
+    * Get ID of Switch
+    * @author Dan Bednarczyk
+    * @return int ID of Switch
+    */
+    @Override
     public int getID() {
         return switchID;
     }
+    
+    /**
+    * Get type of the TrackPiece
+    * @author Dan Bednarczyk
+    * @return PieceType Switch
+    */
+    @Override
     public Global.PieceType getType() {
         return PieceType.SWITCH;
     }
+    
+    /**
+    * Get next TrackPiece
+    * @author Dan Bednarczyk
+    * @return TrackPiece on the other side of the Switch
+    */
+    @Override
     public TrackPiece getNext(TrackPiece previous) {
         if (portA == null || portB == null || portC == null) {
             System.err.println("Switch " + switchID + " is not fully connected. Please check your track configuration.");
@@ -95,6 +151,13 @@ public class Switch implements TrackPiece {
         }
     }
     
+    /**
+    * Compare equality of TrackPiece
+    * @author Dan Bednarczyk
+    * @param otherPiece TrackPiece to compare
+    * @return boolean true if equal, false otherwise
+    */
+    @Override
     public boolean equals(TrackPiece otherPiece) {
         if (this.getType() == otherPiece.getType() && this.switchID == otherPiece.getID()) {
             return true;
@@ -102,11 +165,22 @@ public class Switch implements TrackPiece {
         return false;
     }
     
+    /**
+    * Get String representation of Switch
+    * @author Dan Bednarczyk
+    * @return String representation of Switch containing ID
+    */
     @Override
     public String toString() {
         return "" + switchID;
     }
     
+    /**
+    * Get detailed String representation of Switch
+    * @author Dan Bednarczyk
+    * @return String representation of Switch containing all info
+    */
+    @Override
     public String toStringDetail() {
         StringBuilder sb = new StringBuilder();
         sb.append("Switch: ");
