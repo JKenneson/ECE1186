2dd4a6d750c830886e97e2725230c63da7dcfda8
Dan <Dan@150.212.66.231>
2017-04-05 11:05:34 -0400

    Javadoc'd Yard

--- TrainSystem/src/com/rogueone/trackmodel/Yard.java
+++ TrainSystem/src/com/rogueone/trackmodel/Yard.java
@@ -16,27 +16,64 @@ public class Yard implements TrackPiece {
     
     private static final int YARD_ID = 0;
 
+    /**
+    * Do not call. Use TrackModel.enterTrack(line) instead.
+    * @author Dan Bednarczyk
+    * @param previous TrackPiece 
+    * @return null
+    */
+    @Override
     public TrackPiece getNext(TrackPiece previous) {
         return null;
     }
 
+    /**
+    * Get type of TrackPiece
+    * @author Dan Bednarczyk
+    * @return PieceType Yard
+    */
+    @Override
     public Global.PieceType getType() {
         return PieceType.YARD;
     }
 
+    /**
+    * Get ID of Yard
+    * @author Dan Bednarczyk
+    * @return int indicating Yard ID
+    */
+    @Override
     public int getID() {
         return YARD_ID;
     }
     
+    /**
+    * Get String representation of Yard
+    * @author Dan Bednarczyk
+    * @return String representation of Yard
+    */
     @Override
     public String toString() {
         return "Yard";
     }
     
+    /**
+    * Get detailed String representation of Yard
+    * @author Dan Bednarczyk
+    * @return String representation of Switch containing Yard ID
+    */
+    @Override
     public String toStringDetail() {
         return "Yard (" + YARD_ID + ")";
     }
     
+    /**
+    * Compare equality of TrackPiece
+    * @author Dan Bednarczyk
+    * @param otherPiece TrackPiece to compare
+    * @return boolean true if equal, false otherwise
+    */
+    @Override
     public boolean equals(TrackPiece otherPiece) {
         if (otherPiece.getType() == this.getType() && otherPiece.getID() == this.getID()) {
             return true;
