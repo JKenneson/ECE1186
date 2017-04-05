613d254ee6213c7219268a64ecb2e85f82cc0a6a
Dan <Dan@150.212.66.231>
2017-04-05 10:41:08 -0400

    Javadoc'd Crossings

--- TrainSystem/src/com/rogueone/trackmodel/Crossing.java
+++ TrainSystem/src/com/rogueone/trackmodel/Crossing.java
@@ -11,20 +11,39 @@ package com.rogueone.trackmodel;
  */
 public class Crossing {
     
+    //Default state is raised
     private boolean state = false;
     
+    /**
+    * Get state of crossing
+    * @author Dan Bednarczyk
+    * @return boolean true for lowered, false for raised
+    */
     public boolean getState() {
         return state;
     }
     
+    /**
+    * Set state of crossing
+    * @author Dan Bednarczyk
+    * @param newState boolean true for lowered, false for raised
+    */
     public void setState(boolean newState) {
         state = newState;
     }
     
+    /**
+    * Raise crossing (set state to false)
+    * @author Dan Bednarczyk
+    */
     public void raise() {
         state = false;
     }
     
+    /**
+    * Lower crossing (set state to true)
+    * @author Dan Bednarczyk
+    */
     public void lower() {
         state = true;
     }
