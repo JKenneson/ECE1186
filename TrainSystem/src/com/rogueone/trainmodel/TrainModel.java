5ffc4c8d2d8dea98bcb8dabfd52531b1f5c07e2e
tjpro <tprotivnak@gmail.com>
2017-04-05 10:12:47 -0400

    Updated counter location

--- TrainSystem/src/com/rogueone/trainmodel/TrainModel.java
+++ TrainSystem/src/com/rogueone/trainmodel/TrainModel.java
@@ -196,7 +196,7 @@ public class TrainModel {
      */
     public void createTrainController() {
         //Create a new TrainController object
-        this.trainController = new TrainController(this, null, (byte)this.ctcSetPoint, (short)(this.authority/this.FEET_IN_A_METER), 300, this.trainSystem);
+        this.trainController = new TrainController(this, null, (byte)this.ctcSetPoint, (short)(this.authority/this.FEET_IN_A_METER), 300, Integer.toString(this.trainID),this.trainSystem);
     }
     
     /**
