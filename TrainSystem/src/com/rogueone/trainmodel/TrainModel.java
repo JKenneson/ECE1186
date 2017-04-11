911cdfff6b7eae1e9a072239280a75b044039072
tjpro <tprotivnak@gmail.com>
2017-04-10 20:30:51 -0400

    Stopping at stations again. Uncommented code

--- TrainSystem/src/com/rogueone/trainmodel/TrainModel.java
+++ TrainSystem/src/com/rogueone/trainmodel/TrainModel.java
@@ -550,8 +550,8 @@ public class TrainModel {
                     //Check for beacon and send it to the Track Controller
                     if(this.currBlock.getBeacon() != null) {
                         //System.out.println("BEACON!");
-//                        approachingStationBlock();
-//                        this.trainController.receiveBeacon(this.currBlock.getBeacon());
+                        approachingStationBlock();
+                        this.trainController.receiveBeacon(this.currBlock.getBeacon());
                     }
                 }
                 //Train has reached end of track
