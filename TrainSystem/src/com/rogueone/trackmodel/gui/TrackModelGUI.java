2400e9622910cec6a4107b087dc9274c639de70c
Dan <Dan@150.212.66.231>
2017-04-12 11:26:33 -0400

    Reflected TrackModel failures to CTC

--- TrainSystem/src/com/rogueone/trackmodel/gui/TrackModelGUI.java
+++ TrainSystem/src/com/rogueone/trackmodel/gui/TrackModelGUI.java
@@ -980,7 +980,7 @@ public class TrackModelGUI extends javax.swing.JPanel {
         updateDetailsPanel();
         updateSummaryPanel();
         this.trackModel.getTrainSystem().getTrackControllerHandler().updateTrack(b.getLine().getLineID());
-        //this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
+        this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
     }//GEN-LAST:event_brokenRailFailureButtonActionPerformed
 
     private void trackCircuitFailureButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_trackCircuitFailureButtonActionPerformed
@@ -989,7 +989,7 @@ public class TrackModelGUI extends javax.swing.JPanel {
         updateDetailsPanel();
         updateSummaryPanel();
         this.trackModel.getTrainSystem().getTrackControllerHandler().updateTrack(b.getLine().getLineID());
-        //this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
+        this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
     }//GEN-LAST:event_trackCircuitFailureButtonActionPerformed
 
     private void powerOutageFailureButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_powerOutageFailureButtonActionPerformed
@@ -998,7 +998,7 @@ public class TrackModelGUI extends javax.swing.JPanel {
         updateDetailsPanel();
         updateSummaryPanel();
         this.trackModel.getTrainSystem().getTrackControllerHandler().updateTrack(b.getLine().getLineID());
-        //this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
+        this.trackModel.getTrainSystem().getCTC().updateFailureTable(b);
     }//GEN-LAST:event_powerOutageFailureButtonActionPerformed
 
     private void trackConfigurationResetButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_trackConfigurationResetButtonMouseClicked
